99092ef41046392eaafa53c5d63c9c19bd0a28b9
Niiiimaaaa <78884757+Niiiimaaaa@users.noreply.github.com>
2025-03-10 02:10:45 +0330

    index.js

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const http = require('http');
 const { createProxyServer } = require('http-proxy');
 
 const proxy = createProxyServer({
-    target: 'https://77.37.65.136:10000', // اگه TLS توی x-ui فعاله
-    // target: 'http://77.37.65.136:10000', // اگه TLS توی x-ui خاموشه
-    secure: false // برای قبول گواهی خود-امضا
+    target: 'wss://77.37.65.136:10000',
+    ws: true,
+    secure: false // غیرفعال کردن تأیید گواهی
 });
 
 const server = http.createServer((req, res) => {
-    proxy.web(req, res); // برای HTTP ترافیک عادی
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Proxy is working');
 });
 
 server.on('upgrade', (req, socket, head) => {
-    proxy.ws(req, socket, head); // برای HTTPUpgrade (مثل WebSocket)
+    proxy.ws(req, socket, head);
 });
 
 const PORT = process.env.PORT || 3000;
