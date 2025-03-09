979569cb446a4df9e6c8944febb4ff22643a037b
Niiiimaaaa <78884757+Niiiimaaaa@users.noreply.github.com>
2025-03-09 22:53:44 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const { createProxyServer } = require('http-proxy');
+
+const proxy = createProxyServer({
+    target: 'ws://77.37.65.136:8443', // IP و پورت سرورت
+    ws: true // فعال کردن پشتیبانی WebSocket
+});
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Proxy is working');
+});
+
+server.on('upgrade', (req, socket, head) => {
+    proxy.ws(req, socket, head);
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
